f298a59165329b58e13d9b2bbf9422f376b4ddfe
ephratica <ephratica@foxmail.com>
2022-04-27 21:21:42 +0800

    static pthread 1.0

---
+++ fft_pthread/static1.0.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <queue>
+#include <math.h>
+#include <semaphore.h>
+#include<sys/time.h>
+#include <cassert>
+
+struct timeval tv_begin, tv_end;
+
+using namespace std;
+#define PI acos(-1)
+struct Complex{
+    float x,y;
+    Complex(float _x=0,float _y=0):x(_x),y(_y){}
+};
+Complex operator + (Complex a,Complex b){ return Complex(a.x+b.x , a.y+b.y);}
+Complex operator - (Complex a,Complex b){ return Complex(a.x-b.x , a.y-b.y);}
+Complex operator * (Complex a,Complex b){ return Complex(a.x*b.x-a.y*b.y , a.x*b.y+a.y*b.x);}
+struct Node{
+    Complex *cp;
+    int temp[3];
+    Node(Complex *_cp=NULL,int temp0=0,int temp1=0,int temp2=0) { cp=_cp,temp[0]=temp0,temp[1]=temp1,temp[2]=temp2;}
+};
+void swap(Complex &a,Complex &b){
+    Complex temp=a;
+    a=b;
+    b=temp;
+}
+void change(Complex *y, int len) {
+    int *rev=new int[len];
+    rev[0]=0;
+    for (int i = 0; i < len; ++i) {
+        rev[i] = rev[i >> 1] >> 1;
+        if (i & 1) {
+            rev[i] |= len >> 1;
+        }
+    }
+    for (int i = 0; i < len; ++i) {
+        if (i < rev[i]) {
+            swap(y[i], y[rev[i]]);
+        }
+    }
+    delete[] rev;
+}
+
+
+sem_t	sem_children;
+//pthread_t pth;
+#include <set>
+queue<Node *>pool;
+pthread_mutex_t mutex_for_todo = PTHREAD_MUTEX_INITIALIZER;
+void *fft_pthread(void *a){
+    while (true) {
+        sem_wait(&sem_children);
+        while (pthread_mutex_trylock(&mutex_for_todo) != 0);
+        Node* tt = pool.front();
+        pool.pop();
+        pthread_mutex_unlock(&mutex_for_todo);
+        if(tt== nullptr){
+            return nullptr;
+        }
+        int j=tt->temp[0],h=tt->temp[1],on=tt->temp[2];
+        Complex *y=tt->cp;
+        Complex w(1, 0);
+        Complex wn(cos(2 * PI / h), sin(on * 2 * PI / h));
+        for (int k = j; k < j + h / 2; k++) {
+            Complex u = y[k];
+            Complex t = w * y[k + h / 2];
+            y[k] = u + t;
+            y[k + h / 2] = u - t;
+            w = w * wn;
+        }
+        delete tt;
+        //sem_post(&sem_children);
+    }
+    return nullptr;
+}
+Complex* fft_p(Complex *y, int len, int on) {
+    pthread_mutex_init(&mutex_for_todo, NULL);
+    sem_init(&sem_children, 0, 0);
+    pthread_t threads[7];
+    for (auto& th : threads) {pthread_create(&th, nullptr, fft_pthread, nullptr);}
+    for (auto& th : threads) {pthread_detach(th);}
+    change(y, len);
+    for (int h = 2; h <= len; h <<= 1) {
+        pthread_mutex_lock(&mutex_for_todo);
+        for (int j = 0; j < len; j += h) {
+            Node *temp=new Node(y,j,h,on);
+            pool.push(temp);
+            sem_post(&sem_children);
+        }
+        for(int i=0;i<7;i++) {
+            pool.push(nullptr);
+            sem_post(&sem_children);
+        }
+        pthread_mutex_unlock(&mutex_for_todo);
+    }
+    if (on == -1) {
+        for (int i = 0; i < len; i++) {
+            y[i].x /= len;y[i].y/=len;
+        }
+    }
+    return y;
+}
+Complex* fft(Complex *y, int len, int on) {
+    change(y, len);
+    for (int h = 2; h <= len; h <<= 1) {
+        Complex wn(cos(2 * PI / h), sin(on * 2 * PI / h));
+        for (int j = 0; j < len; j += h) {
+            Complex w(1, 0);
+            for (int k = j; k < j + h / 2; k++) {
+                Complex u = y[k];
+                Complex t = w * y[k + h / 2];
+                y[k] = u + t;
+                y[k + h / 2] = u - t;
+                w = w * wn;
+            }
+        }
+    }
+    if (on == -1) {
+        for (int i = 0; i < len; i++) {
+            y[i].x /= len;y[i].y/=len;
+        }
+    }
+    return y;
+}
+Complex y[1<<25],yy[1<<25];
+int main(){
+
+    for (int i = 0; i < (1<<20); i++) {
+        y[i].x = yy[i].x = rand();
+        y[i].y = yy[i].y = rand();
+    }
+    for (int i = 0; i < 10; ++i)
+        fft(yy,1<<20,1);
+    gettimeofday(&tv_begin,NULL);
+    fft(yy,1<<20,1);
+    gettimeofday(&tv_end,NULL);
+    long long sb=tv_begin.tv_sec*(1e6)+tv_begin.tv_usec,se=tv_end.tv_sec*(1e6)+tv_end.tv_usec;
+    printf("normal: %lld\n",se-sb);
+    gettimeofday(&tv_begin,NULL);
+    fft_p(y,1<<20,1);
+    gettimeofday(&tv_end,NULL);
+    sb=tv_begin.tv_sec*(1e6)+tv_begin.tv_usec,se=tv_end.tv_sec*(1e6)+tv_end.tv_usec;
+    printf("pyhread: %lld\n",se-sb);
+    return 0;
+}
